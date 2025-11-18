c5cb222e6a893883694568c0f81783df1886ffe2
lab_services_student <st10183958@vcconnect.edu.za>
2025-11-18 18:22:35 +0200

    Add project files.

---
+++ PROG2112ClaimsPOE/Models/ClaimModel.cs
@@ -0,0 +1,16 @@
+﻿namespace PROG2112ClaimsPOE.Models
+{
+    public class ClaimModel
+    {
+    public string Id { get; set; }
+    public string LectureName { get; set; }
+    public string LectureSurname { get; set; }
+    public string SubjectName { get; set; }
+    public string LecturerEmail { get; set; }
+    public string SubjectCode { get; set; }
+    public string CampusLocation { get; set; }
+    public string HoursWorked { get; set; }
+    public string Message { get; set; }
+    public string Statues { get; set; }
+    }
+}
